7b98853a41eadbed992edc373c209cab34ce492e
opilane <opilane@HP-CZC6297B9D>
2020-12-10 10:48:34 +0200

    Kontakt andmed

--- Kontakt_ruhm/Form1.Designer.cs
+++ Kontakt_ruhm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Kontakt_ruhm
 {
-    partial class Form1
+    partial class form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "form1";
+            this.Text = "Kontaktandmed klassijuhataja jaoks";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
